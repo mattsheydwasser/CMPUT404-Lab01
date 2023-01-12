8b72755ed15806b2ad1978cdffc745c8b29ccafd
mattsheydwasser <sheydwas@ualberta.ca>
2023-01-11 17:27:16 -0700

    removed old comments, cleaned up spacing

--- lab1.py
+++ lab1.py
@@ -6,14 +6,13 @@ def main():
     print('Requests library version: ' + requests.__version__)
 
 
-    # get the Google home page, display the intial "moved" status, then the "ok" after redirect
+    # get the Google home page and print it out
     res = requests.get('http://google.com')
-    
     print('\nGET result: \n\n' + res.text)
+
 
     # get the raw file of this script off of GitHub, and print it out
     raw = requests.get('https://raw.githubusercontent.com/mattsheydwasser/CMPUT404-Lab01/master/lab1.py')
-
     print('\nSource code of this file: \n\n'+raw.text)
 
     
