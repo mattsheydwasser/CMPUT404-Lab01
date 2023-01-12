f4cb36fd1ad88b596a252cb978eb4f30c11a53cb
mattsheydwasser <sheydwas@ualberta.ca>
2023-01-11 17:25:17 -0700

    printed text of get result rather then status code

--- lab1.py
+++ lab1.py
@@ -9,9 +9,7 @@ def main():
     # get the Google home page, display the intial "moved" status, then the "ok" after redirect
     res = requests.get('http://google.com')
     
-    print('Initial status code: ' + str(res.history[0].status_code))
-    print('Status code after auto-redirect: '+ str(res.status_code))
-
+    print('\nGET result: \n\n' + res.text)
 
     # get the raw file of this script off of GitHub, and print it out
     raw = requests.get('https://raw.githubusercontent.com/mattsheydwasser/CMPUT404-Lab01/master/lab1.py')
